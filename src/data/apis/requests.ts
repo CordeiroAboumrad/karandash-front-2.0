d8c1296f61931f161bfea0e7317e4180c141eeab
JeanAboumrad <jean.aboumrad@dataprev.gov.br>
2026-02-16 23:48:17 -0300

    Added new requests and types for the parameters.

--- src/data/apis/requests.ts
+++ src/data/apis/requests.ts
@@ -1,17 +1,84 @@
 import { karandashClient } from "../config/axios";
-
-
-export const getAllProducts = async () => {
-    const res = await karandashClient.get("/product/all-products");
-    return res.data;
-};
-
-export const getProductImage = async (productId: string) => {
-    const res = await karandashClient.get(`/product/retrieve-image/${productId}`);
-    return res.data;
-};
+import { Artist, Customer, Product } from "./types";
 
 export const getArtists = async () => {
-    const res = await karandashClient.get('/artists');
-    return res.data;
-}
+  const res = await karandashClient.get("/artists");
+  return res.data;
+};
+
+export const createArtist = async (artistData: Artist) => {
+  const res = await karandashClient.post("/artists", artistData);
+  return res.data;
+};
+
+export const adjustArtist = async (artistData: Artist) => {
+  const res = await karandashClient.put("/artists", artistData);
+  return res.data;
+};
+
+export const login = async (email: string, password: string) => {
+  const res = await karandashClient.post("/auth/login", { email, password });
+  return res.data;
+};
+
+export const registerCustomer = async (customerData: Customer) => {
+  const res = await karandashClient.post(
+    "/customer/input-customer",
+    customerData,
+  );
+  return res.data;
+};
+
+export const getCustomer = async (id: string) => {
+  const res = await karandashClient.get(`/customers/${id}`);
+  return res.data;
+};
+
+export const createProduct = async (productData: Product) => {
+  const res = await karandashClient.post(
+    "/product/create-product",
+    productData,
+  );
+  return res.data;
+};
+
+export const getProductByIdBody = async (id: string) => {
+  const res = await karandashClient.get("/product/retrieve-product", {
+    data: {
+      productId: id,
+    },
+  });
+  return res.data;
+};
+
+export const getAllProducts = async () => {
+  const res = await karandashClient.get("/product/all-products");
+  return res.data;
+};
+
+export const saveProductImage = async (imageData: FormData) => {
+  const res = await karandashClient.post(`/product/save-image`, {
+    file: imageData.get("file") as File,
+  });
+  return res.data;
+};
+
+export const getProductImage = async (productId: number) => {
+  const res = await karandashClient.get(`/product/retrieve-image/${productId}`);
+  return res.data;
+};
+
+export const getProductById = async (id: number) => {
+  const res = await karandashClient.get(`/productretrieve-first/${id}`);
+  return res.data;
+};
+
+export const deleteProduct = async (id: number, displayPosition: number) => {
+  const res = await karandashClient.delete("/product/delete-product", {
+    data: {
+      productId: id,
+      displayPosition: displayPosition,
+    },
+  });
+  return res.data;
+};
---
+++ src/data/apis/types.ts
@@ -0,0 +1,26 @@
+export interface Artist {
+  artistName: string;
+  dateOfBirth: string;
+  placeOfBirth: string;
+  history: string;
+}
+
+export interface Customer {
+  customerName: string;
+  address: string;
+  email: string;
+}
+
+export interface Product {
+  description: string;
+  title: string;
+  company: string;
+  status: string;
+  type: string;
+  keywords: string;
+  artTechnique: string;
+  acquisitionCost: number;
+  value: number;
+  productYear: string;
+  measurements: string;
+}
