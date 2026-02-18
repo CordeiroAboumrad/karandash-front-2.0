8a1c2415b1595c39e78420559ea4649601c8428d
JeanAboumrad <jean.aboumrad@dataprev.gov.br>
2026-02-18 18:26:05 -0300

    Adjusted root component, in order to fix both header and footer in the pages.

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,14 +1,15 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import styles from "./Root.module.css";
 
 import { Main } from "./Main";
 
 export const Root = () => {
   return (
-    <>
+    <div className={styles.root}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 };
